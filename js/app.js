4386b164b06965ea2cd1e06b9e12e2f11eb2dd3c
Arturs Vasiljevs <freelancerarthur@gmail.com>
2018-07-03 21:10:31 +0300

    Add - social media information

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const data = [
+  {
+    name: 'Duolingo',
+    description: 'Learn a language for free. Forever.',
+    url: 'duolingo.com/vasiljevs'
+  },
+  {
+    name: 'Endomondo',
+    description: 'Free your endorphins running, walking, cycling and more.',
+    url: 'endomondo.com/profile/34596180'
+  },
+  {
+    name: 'Goodreads',
+    description: 'Meet your next favorite book.',
+    url: 'goodreads.com/vasiljevs'
+  },
+  {
+    name: 'GitHub',
+    description: 'The world\'s leading software development platform.',
+    url: 'github.com/vasiljevs'
+  },
+  {
+    name: 'LinkedIn',
+    description: 'The world\'s largest professional network.',
+    url: 'linkedin.com/in/vasiljevs'
+  },
+  {
+    name: 'Meetup',
+    description: 'Meet people near you who share your interests.',
+    url: 'meetup.com/members/217255260'
+  },
+  {
+    name: 'TypeRacer',
+    description: 'Test your typing speed and learn to type faster.',
+    url: 'data.typeracer.com/pit/profile?user=vasiljevs'
+  }
+];
